39a66f6425f2b130c3ea0c90ba36454daa25c071
Isabel Trahan <isabel@mitre.org>
2019-09-24 08:40:11 -0400

    updated technique page layout

--- theme/scripts/navigator-link.js
+++
@@ -1,8 +0,0 @@
-function setNavLinkHref(id, path){
-    if(id == "mobile-layer"){
-        document.getElementById(id).href = "https://mitre-attack.github.io/attack-navigator/mobile/#layerURL=" + encodeURIComponent(window.location.protocol + "//" + window.location.hostname + path);
-    }
-    else{
-        document.getElementById(id).href = "https://mitre-attack.github.io/attack-navigator/enterprise/#layerURL=" + encodeURIComponent(window.location.protocol + "//" + window.location.hostname + path);
-    }
-}
