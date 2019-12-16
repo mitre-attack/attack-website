31a210ed82fd33e1fb0f6171dbc379a3c5bd3303
Isabel Trahan <isabel@mitre.org>
2019-12-16 15:28:52 -0500

    Add additional matrix controls. TODO help button

--- attack-theme/static/scripts/matrix.js
+++ attack-theme/static/scripts/matrix.js
@@ -3,3 +3,23 @@ function matrix_toggle_technique(tactic_id, technique_id) {
     $(".subtechniques--" + joined).toggleClass("hidden");
     $(".sidebar--" + joined).toggleClass("expanded");
 }
+
+function matrix_toggle_all(visible) {
+    if (visible) {
+        $(".sidebar").addClass("expanded");
+        $(".subtechniques-container").removeClass("hidden");
+    } else {
+        $(".sidebar").removeClass("expanded");
+        $(".subtechniques-container").addClass("hidden");
+    }
+}
+
+function show_side_matrix() {
+    $(".matrix-type.side").removeClass("d-none");
+    $(".matrix-type.flat").addClass("d-none");
+}
+
+function show_flat_matrix() {
+    $(".matrix-type.flat").removeClass("d-none");
+    $(".matrix-type.side").addClass("d-none");
+}
