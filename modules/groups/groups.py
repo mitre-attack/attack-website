d725699ee88a370f32943af3a8c571976d5dc59e
Davila <IDAVILA@MITRE.ORG>
2020-02-04 07:52:32 -0500

    Updating groups module

--- modules/groups/groups.py
+++ modules/groups/groups.py
@@ -1,11 +1,25 @@
 from . import groups_config
+from modules import util
+import time
 
 def generate_groups():
     """ Generate groups, return True if groups was generated,
         False if nothing was generated
     """
 
-    print("DO stuff!")
+    # First call to lazy loading
+    test_start = time.time()
+    util.build_helpers.get_malware_used_by_groups()
+    test_end = time.time()
+    test_time = test_end - test_start
+    print(test_time)
+
+    # Second call to lazy loading
+    test_start = time.time()
+    util.build_helpers.get_malware_used_by_groups()
+    test_end = time.time()
+    test_time = test_end - test_start
+    print(test_time)
 
     return True
    
