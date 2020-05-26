0d74ad0a0ba3a2fe231c36eeb2606075b856059a
Isabel Tuson <isabel@mitre.org>
2020-05-26 13:28:38 -0400

    fix bug where multiple imports of settings.js would cause errors due to let being picky

--- modules/tour/tour_config.py
+++ modules/tour/tour_config.py
@@ -3,4 +3,4 @@ from string import Template
 module_name = "tour"
 priority = 15
 
-js_tour_settings = Template("let tour_steps = ${tour_steps};\n")
+js_tour_settings = Template("var tour_steps = ${tour_steps};\n")
