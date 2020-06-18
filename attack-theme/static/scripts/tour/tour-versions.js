37379d04cb5494cc263fe676cf97ba214b2107f3
Ian Davila <ianm.davila@gmail.com>
2020-06-18 10:10:59 -0400

    Adding versioning feature to tour

--- attack-theme/static/scripts/tour/tour-versions.js
+++ attack-theme/static/scripts/tour/tour-versions.js
@@ -1,58 +1,66 @@
 //is the user doing a tour of the entire site, or just this module?
 isSiteTour = window.location.href.includes("?tour=true");
 
+let lastStepReached = false;
+
 let tourSteps = [
     {
         orphan: true,
         backdrop: false,
         title: "Versions of ATT&CK",
-        content: "Versions of ATT&CK website are preserved in this page.",
+        content: "Versions of the ATT&CK website are preserved in this page."
+    },
+    {
+        element: "#version-column",
+        placement: "top",
+        backdrop: false,
+        title: "Versions of ATT&CK",
+        content: "You can view past versions of the website by clicking on the links under this column."
+    },
+    {
+        element: "#data-column",
+        placement: "top",
+        backdrop: false,
+        title: "Versions of ATT&CK",
+        content: "The STIX bundles are also preserved by versions. The latest release of each major version can be found under the Data column."
+    },
+    {
+        element: "#release-notes-column",
+        placement: "top",
+        backdrop: false,
+        title: "Versions of ATT&CK",
+        content: "Each major release is accompanied with release notes that details the updates. View the release notes under this column, alternatively they can be found <a href='" + base_url + "resources/updates'>here</a>."
+    },
+    {
+        orphan: true,
+        backdrop: false,
+        title: "End of tour",
+        content: "We hope you have enjoyed this tour of the sub-techniques features of the ATT&CK website. If you have any feedback or suggestions, please visit <a href='" + base_url + "contact'>the contact page</a> to get in touch.",
+        onShow: function() {
+            lastStepReached = true;
+        },
+        onNext: function() {
+            window.location.href = base_url;
+        }
     }    
 ]
 
-// if (isSiteTour && tour_steps['relationships']['step1'] != 'undefined') tourSteps.push({
-//     element: "#uses-" + tour_steps['relationships']['step1'],
-//     placement: "left",
+
+// if (isSiteTour) tourSteps.push({
+//     orphan: true,
 //     backdrop: false,
-//     title: "Technique tables",
-//     content: "In cases where a technique exists in the table but not its sub-techniques, the row format is unchanged."
+//     title: "End of tour",
+//     content: "We hope you have enjoyed this tour of the sub-techniques features of the ATT&CK website. If you have any feedback or suggestions, please visit <a href='" + base_url + "contact'>the contact page</a> to get in touch.",
+//     onShow: function() {
+//         lastStepReached = true;
+//     },
+//     onNext: function() {
+//         window.location.href = base_url;
+//     }
 // })
-
-// if (isSiteTour && tour_steps['relationships']['step2'] != 'undefined') tourSteps.push({
-//     element: "#uses-" + tour_steps['relationships']['step2'],
-//     placement: "left",
-//     backdrop: false,
-//     title: "Technique tables",
-//     content: "In cases where both the technique and its sub-techniques exists in the table, the sub-techniques are shown nested beneath their parent in the ID column."
-// })
-
-// if (isSiteTour && tour_steps['relationships']['step3'] != 'undefined') tourSteps.push({
-//     element: "#uses-" + tour_steps['relationships']['step3'],
-//     placement: "left",
-//     backdrop: false,
-//     title: "Technique tables",
-//     content: "In cases where a sub-technique exists in the table but not the parent technique, the parent technique row is omitted entirely."
-// })
-
-let lastStepReached = false;
-
-if (isSiteTour) tourSteps.push({
-    orphan: true,
-    backdrop: false,
-    title: "End of tour",
-    content: "We hope you have enjoyed this tour of the sub-techniques features of the ATT&CK website. If you have any feedback or suggestions, please visit <a href='" + base_url + "contact'>the contact page</a> to get in touch.",
-    onShow: function() {
-        lastStepReached = true;
-    },
-    onNext: function() {
-        window.location.href = base_url;
-    }
-})
 
 let tour = new Tour({
-    container: "#tab-content",
     steps: tourSteps,
-    container: "#v-tabContent",
     storage: false, //no resuming tour if the page is reloaded.
     framework: 'bootstrap4',   // set Tourist to use BS4 compatibility
     showProgressBar: !isSiteTour,
