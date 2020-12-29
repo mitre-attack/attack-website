14041f2a373d5f1cff3e16e079b238750d5a9ce2
Isabel Tuson <isabel@mitre.org>
2020-12-29 14:20:22 -0500

    update banner

--- theme/scripts/settings.js
+++ theme/scripts/settings.js
@@ -1,3 +1,3 @@
 var base_url = "/";
-var build_uuid = "7bf966af-2e03-41ee-abc9-fc245edff515";
+var build_uuid = "1c52fce5-4135-4e6b-bb33-7e9957c25393";
 var tour_steps = {'matrix': 'matrices/enterprise', 'technique': 'techniques/T1548', 'subtechnique': 'techniques/T1548/001', 'relationships': {'step1': 'T1016', 'step3': ['T1003-003', 3], 'step2': ['T1110', 2], 'obj_id': 'software/S0488'}};
--- versions/v8/theme/scripts/settings.js
+++ versions/v8/theme/scripts/settings.js
@@ -1,3 +1,3 @@
 var base_url = "/versions/v8/";
-var build_uuid = "7bf966af-2e03-41ee-abc9-fc245edff515";
+var build_uuid = "1c52fce5-4135-4e6b-bb33-7e9957c25393";
 var tour_steps = {};
