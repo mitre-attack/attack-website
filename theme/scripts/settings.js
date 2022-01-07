3190b0fc41932a964a57108e5bb6bf6596df96b1
idavila <idavila>
2022-01-07 15:55:12 -0500

    v3.5.0

--- theme/scripts/settings.js
+++ theme/scripts/settings.js
@@ -1,3 +1,3 @@
 var base_url = "/";
-var build_uuid = "532afa67-fe12-4e8a-9ced-6a7f400ebe15";
+var build_uuid = "b612f436-ef02-405a-85e3-f5ef6b54a678";
 var tour_steps = {'matrix': 'matrices/enterprise', 'technique': 'techniques/T1548', 'subtechnique': 'techniques/T1548/001', 'relationships': {'step1': 'T1125', 'step3': ['T1021-004', 3], 'step2': ['T1059', 4], 'obj_id': 'groups/G0046'}};
--- versions/v10/theme/scripts/settings.js
+++ versions/v10/theme/scripts/settings.js
@@ -1,3 +1,3 @@
 var base_url = "/versions/v10/";
-var build_uuid = "532afa67-fe12-4e8a-9ced-6a7f400ebe15";
+var build_uuid = "b612f436-ef02-405a-85e3-f5ef6b54a678";
 var tour_steps = {};
