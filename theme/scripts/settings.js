5500e70c1d3d4127d5f45b029b22951c639e9d97
Jared Ondricek <jondricek@mitre.org>
2022-03-11 09:49:03 -0600

    update banner

--- theme/scripts/settings.js
+++ theme/scripts/settings.js
@@ -1,3 +1,3 @@
 var base_url = "/";
-var build_uuid = "b612f436-ef02-405a-85e3-f5ef6b54a678";
+var build_uuid = "a47a797f-8cb4-4867-a406-927e06bc6e62";
 var tour_steps = {'matrix': 'matrices/enterprise', 'technique': 'techniques/T1548', 'subtechnique': 'techniques/T1548/001', 'relationships': {'step1': 'T1125', 'step3': ['T1021-004', 3], 'step2': ['T1059', 4], 'obj_id': 'groups/G0046'}};
--- versions/v10/theme/scripts/settings.js
+++ versions/v10/theme/scripts/settings.js
@@ -1,3 +1,3 @@
 var base_url = "/versions/v10/";
-var build_uuid = "b612f436-ef02-405a-85e3-f5ef6b54a678";
+var build_uuid = "a47a797f-8cb4-4867-a406-927e06bc6e62";
 var tour_steps = {};
