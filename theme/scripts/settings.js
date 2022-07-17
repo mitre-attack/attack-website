dbc573b0eab418132bef0d7db16eef6e8a625ae5
jondricek <jondricek@users.noreply.github.com>
2022-07-17 23:49:53 +0000

    deploy: 9ac41c228c0478847c1df1a66d801a4d0afb5d60

--- theme/scripts/settings.js
+++ theme/scripts/settings.js
@@ -1,3 +1,3 @@
 var base_url = "/";
-var build_uuid = "5415ea04-dd0f-47f4-82f5-9b001524041d";
+var build_uuid = "2a13cefd-5a90-407b-8696-8e9e04d33d14";
 var tour_steps = {'matrix': 'matrices/enterprise', 'technique': 'techniques/T1548', 'subtechnique': 'techniques/T1548/002', 'relationships': {'step3': ['T1003-001', 5], 'step2': ['T1555', 3], 'obj_id': 'software/S0349'}};
--- versions/v11/theme/scripts/settings.js
+++ versions/v11/theme/scripts/settings.js
@@ -1,3 +1,3 @@
 var base_url = "/versions/v11/";
-var build_uuid = "5415ea04-dd0f-47f4-82f5-9b001524041d";
+var build_uuid = "2a13cefd-5a90-407b-8696-8e9e04d33d14";
 var tour_steps = {};
