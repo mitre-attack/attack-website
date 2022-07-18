3d24d7171811e51f14323a32f848147901141470
jondricek <jondricek@users.noreply.github.com>
2022-07-18 00:28:11 +0000

    deploy: 3d6954d97229a4e7d87492be03f7dec78e039816

--- theme/scripts/settings.js
+++ theme/scripts/settings.js
@@ -1,3 +1,3 @@
 var base_url = "/";
-var build_uuid = "2a13cefd-5a90-407b-8696-8e9e04d33d14";
+var build_uuid = "2e102b2f-4e78-46e1-b001-342c470f830a";
 var tour_steps = {'matrix': 'matrices/enterprise', 'technique': 'techniques/T1548', 'subtechnique': 'techniques/T1548/002', 'relationships': {'step3': ['T1003-001', 5], 'step2': ['T1555', 3], 'obj_id': 'software/S0349'}};
