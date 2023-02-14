fbd22d1b404832405fdf1b9f563e00b40ce567c3
Sean Sica <23294618+seansica@users.noreply.github.com>
2023-02-14 16:38:16 -0500

    removed dynamically generated settings; moved base_url, page_limit, and buffer to dedicated settings module

--- attack-search/src/settings.js
+++ attack-search/src/settings.js
@@ -1,8 +1,7 @@
-const base_url = "/attack-website/";
-const build_uuid = "507e286e-7d49-4c15-abf3-55d7a1aff744"; // TODO determine why this is necessary and provide explanation in comments here
-const tour_steps = {
-    'matrix': 'matrices/enterprise',
-    'technique': 'techniques/T1548',
-    'subtechnique': 'techniques/T1548/002',
-    'relationships': {'step3': ['T1003-004', 5], 'step2': ['T1555', 3], 'obj_id': 'software/S0349'}
-};
+export let base_url = "";
+
+// number of results per page
+export let page_limit = 5;
+
+// 2* buffer is roughly the size of the result preview
+export let buffer = 200;
