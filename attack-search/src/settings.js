8a111e8e4e3c2f190b3e3ff1b6a2e213871e9e8c
Sean Sica <23294618+seansica@users.noreply.github.com>
2023-04-11 13:41:37 -0400

    Migrated pageLimit and buffer to SearchService; removed un-used *_INDEX_KEY constants

--- attack-search/src/settings.js
+++ attack-search/src/settings.js
@@ -1,11 +1,1 @@
 module.exports.baseURL = ''; // TODO migrate from base_url (generated via Pelican)
-
-// number of results per page
-module.exports.pageLimit = 5; // TODO migrate from page_limit (generated via Pelican)
-
-// 2* buffer is roughly the size of the result preview
-module.exports.buffer = 200;
-
-module.exports.ATTACK_INDEX_KEY = 'index_helper';
-module.exports.TITLE_INDEX_KEY = 'index_helper_title';
-module.exports.CONTENT_INDEX_KEY = 'index_helper_content';
