1a883afeeeaf957ba1c71e2ea6e695195518e944
Sean Sica <23294618+seansica@users.noreply.github.com>
2023-04-28 15:54:09 -0400

    Migrate CommonJS to ESM

---
+++ attack-search/src/components.mjs
@@ -0,0 +1,26 @@
+import $ from 'jquery';
+
+// overlay container
+export const searchOverlay = $('#search-overlay');
+
+// button in header to open search
+export const searchButton = $('#search-button');
+
+// icon shown in search button
+export const searchIcon = $('#search-icon');
+
+// button to close search
+export const closeButton = $('#close-search-icon');
+
+// text input on search page
+export const searchInput = $('#search-input');
+
+// body of search results
+export const searchBody = $('#search-body');
+
+// button to show more results
+export const loadMoreResults = $('#load-more-results');
+export const loadMoreResultsButton = $('#load-more-results-button');
+
+// search parsing icon
+export const searchParsingIcon = $('#search-parsing-icon');
