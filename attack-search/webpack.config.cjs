8d050bd94d7075617d7b86fb6f55af452a69e249
Sean Sica <23294618+seansica@users.noreply.github.com>
2023-04-28 16:03:40 -0400

    Modify webpack.config.cjs to support service worker

--- attack-search/webpack.config.cjs
+++ attack-search/webpack.config.cjs
@@ -1,32 +1,45 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
+const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = (env, argv) => {
     const isProduction = argv.mode === 'production';
 
     return {
         entry: {
-            'search_bundle': './src/index.js',
-            'search_bundle.worker': './src/attack-index.js'
+            'search_bundle': './src/index.mjs',
+            'search_bundle.worker': './src/search-service.worker.mjs'
         },
         output: {
             path: path.resolve(__dirname, 'dist'),
-            filename: '[name].js'
+            filename: '[name].js',
+            publicPath: '/',
         },
+        plugins: [
+            new InjectManifest({
+                swSrc: './src/search-service.worker.mjs',
+                swDest: 'search_bundle.worker.js',
+            }),
+        ],
         mode: isProduction ? 'production' : 'development',
         target: 'web',
         module: {
             rules: [
                 {
-                    test: /\.js$/,
+                    test: /\.m?js$/,
                     use: {
                         loader: 'babel-loader',
                         options: {
                             presets: ['@babel/preset-env'],
                             comments: false
                         }
-                    }
-                }
+                    },
+                    type: 'javascript/auto',
+                },
+                {
+                    test: /\.cjs$/,
+                    type: 'javascript/auto',
+                },
             ]
         },
         optimization: isProduction
