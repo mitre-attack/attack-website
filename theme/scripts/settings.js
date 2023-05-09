b35e688ca1b07fea89beb64b4af624ed44c5af74
Jared Ondricek <jondricek@mitre.org>
2023-05-09 10:22:15 -0500

    manual deploy of v4.0.3

--- theme/scripts/settings.js
+++ theme/scripts/settings.js
@@ -1,3 +1,3 @@
 var base_url = "/";
-var build_uuid = "e3f90f9a-3733-42a5-8b59-e7baa4cb3050";
-var tour_steps = {'matrix': 'matrices/enterprise', 'technique': 'techniques/T1548', 'subtechnique': 'techniques/T1548/002', 'relationships': {'step3': ['T1059-003', 3], 'step1': 'T1047', 'step2': ['T1027', 3], 'obj_id': 'software/S0673'}};
+var build_uuid = "1bfcea6c-d2c7-4bd5-b76a-f5ad4619fbf4";
+var tour_steps = {'matrix': 'matrices/enterprise', 'technique': 'techniques/T1548', 'subtechnique': 'techniques/T1548/002', 'relationships': {'step3': ['T1003-008', 5], 'step2': ['T1555', 3], 'obj_id': 'software/S0349'}};
--- versions/v13/theme/scripts/settings.js
+++ versions/v13/theme/scripts/settings.js
@@ -1,3 +1,3 @@
 var base_url = "/versions/v13/";
-var build_uuid = "e3f90f9a-3733-42a5-8b59-e7baa4cb3050";
+var build_uuid = "1bfcea6c-d2c7-4bd5-b76a-f5ad4619fbf4";
 var tour_steps = {};
