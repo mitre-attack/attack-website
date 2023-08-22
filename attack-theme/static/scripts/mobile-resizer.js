3692ef90c475cdd5d3e87feed168fb45e477f530
adpare <apare@mitre.org>
2023-08-22 10:26:57 -0400

    code cleanup

--- attack-theme/static/scripts/mobile-resizer.js
+++ attack-theme/static/scripts/mobile-resizer.js
@@ -1,16 +1,14 @@
 const mediaQuery = window.matchMedia('(max-width: 47.9875rem)')
 
-    function mobileSidenav(e) {
-    // Check if the media query is true
-    if (e.matches) {
-        // Then log the following message to the console
-        $('#sidebar-collapse').collapse('hide')
-    }
-    else{
-        $('#sidebar-collapse').collapse('show')
-    }
-    }
-    $(document).ready(function() {
-        mobileSidenav(mediaQuery)
-    });
-    mediaQuery.addEventListener('change', mobileSidenav)
+function mobileSidenav(e) {
+if (e.matches) {
+    $('#sidebar-collapse').collapse('hide')
+}
+else{
+    $('#sidebar-collapse').collapse('show')
+}
+}
+$(document).ready(function() {
+    mobileSidenav(mediaQuery)
+});
+mediaQuery.addEventListener('change', mobileSidenav)
