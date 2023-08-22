edde48968a4501d82b9e3f36b6f9bca57668e397
adpare <apare@mitre.org>
2023-08-22 15:25:45 -0400

    manage spacing

--- attack-theme/static/scripts/resizer.js
+++ attack-theme/static/scripts/resizer.js
@@ -37,14 +37,15 @@ resizer.addEventListener("mousedown", resizeSidebar_mousedownHandler);
 const mediaQuery = window.matchMedia('(max-width: 47.9875rem)')
 
 function mobileSidenav(e) {
-if (e.matches) {
-    $('#sidebar-collapse').collapse('hide')
-}
-else{
-    $('#sidebar-collapse').collapse('show')
-}
+  if (e.matches) {
+      $('#sidebar-collapse').collapse('hide')
+  }
+  else{
+      $('#sidebar-collapse').collapse('show')
+  }
 }
 $(document).ready(function() {
-    mobileSidenav(mediaQuery)
+  mobileSidenav(mediaQuery)
 });
+
 mediaQuery.addEventListener('change', mobileSidenav)
