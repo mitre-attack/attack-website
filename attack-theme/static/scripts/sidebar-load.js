602e67a2cc26f77a96721aef1615f61b2b101ec7
adpare <apare@mitre.org>
2023-09-11 19:49:24 -0400

    first attempt at loading sidebar

--- attack-theme/static/scripts/sidebar-load.js
+++ attack-theme/static/scripts/sidebar-load.js
@@ -1,16 +1,30 @@
 $(document).ready(function () { 
+console.log("by");
+});
 
-    let active_id = ""
+    console.log("hi");
+    $("#sidebars").load("/docs/sidebar.html", function() {
+        // put the code here that you want to run when the .load() 
+        // has completed and the content is available
+        // Or, you can call a function from here
+        let active_id = ""
 
     // Check if referrer was from the same domain
     // If not, set update to false
     // This will remove unwanted behavior that appears when
     // jumping from one domain to another. E.g. techniques to matrices
-    let current_modules = window.location.pathname.split("/");
+    $(".sidenav-head").each(function () {
+        let check = localStorage.getItem("need_update");
+        if($(this)[0].id == localStorage.getItem("new_active_id") && check === true){
+        $(this).addClass("active");
+        }
+    });
 
     var sidebar = document.querySelector(".sidebar");
     var sidebarSize = localStorage.getItem('sidebarWidth');
     sidebar.style.width = sidebarSize;
+
+    let current_modules = window.location.pathname.split("/");
 
     if (document.referrer) {
         // Loop through the modules in case page is hosted from different 
@@ -29,65 +43,27 @@ $(document).ready(function () {
     else {
         localStorage.setItem("need_update", false);
     }
-    
+
     // Update panes
     if (localStorage.getItem("need_update") === "true") {
         // Get active id from storage and remove update
         active_id = localStorage.getItem("new_active_id").split("-")
         localStorage.setItem("need_update", false);
+        $(this).addClass("active");
     }
     else {
         // Set active id to first element that is active
         // This will open the first one in the case that there are multiple
         $(".sidenav-head.active:first").each(function () {
             active_id = $(this).attr('id').split("-");
+            $(this).addClass("active");
         });
     }
-
-    // Open necessary panes
-    $(".expand-button").each(function () {
-        let tree = "";
-        for (let i = 0; i < active_id.length; i++) {
-            tree += active_id[i] + "-";
-            if ($(this).attr('id') === tree+"header") {
-                $(this).addClass("notransition");
-                $(this).removeClass("collapsed");
-            }
-        }
-    });
-
-    $(".sidenav-body.collapse").each(function () {
-        let tree = "";
-        for (let i = 0; i < active_id.length; i++) {
-            tree += active_id[i] + "-";
-            if ($(this).attr('id') === tree+"body") {
-                $(this).addClass("show");
-            }
-        }
-    });
 
     // If this method is called then the user is clicking on the sidenav
     // Set update to true and set the new id to one that was clicked
     $(".sidenav-head").click(function () {
         localStorage.setItem("need_update", true);
         localStorage.setItem("new_active_id", $(this).attr('id'));
-        $(this).addClass("active");
     });
-
-    // If this method is called, then the user has clicked on a link in the subtechniques dropdown table.
-    // Sets update to true and creates a new_active_id from the active_id so that the original domain-tactic_id-tech_id path is retained.
-    // This ensures that the sidenav opens the correct tactic (given the context) for the subtechnique that was clicked.
-    $(".subtechnique-table-item").click(function () {
-        let new_active_id = active_id;
-        new_active_id.push($(this).attr('data-subtechnique_id'));
-        localStorage.setItem("need_update", true);
-        localStorage.setItem("new_active_id", new_active_id.join("-"));
-    });
-
-    // Remove notransition class when expand button is clicked
-    $(".expand-button.notransition").click(function () {
-        $(this).removeClass("notransition");
-        console.log("removed class");
-    });
-
 });
