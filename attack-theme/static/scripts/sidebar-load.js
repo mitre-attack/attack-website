f61a152df26413fd6f80e8a80618199ce3eabd96
adpare <apare@mitre.org>
2023-09-15 00:18:04 -0400

    progress on active class

--- attack-theme/static/scripts/sidebar-load.js
+++ attack-theme/static/scripts/sidebar-load.js
@@ -1,1 +1,16 @@
-$("#sidebars").load("/resources/sidebar-check");
+$("#sidebars").load("/resources/sidebar-check", function() {
+    var navElements = document.querySelectorAll('.sidenav-head > a');
+    var winlocation;
+    navElements.forEach(function(element){
+    if(!window.location.href.endsWith("/")){
+        winlocation = window.location.href + "/";
+    }
+    else{
+        winlocation = window.location.href
+    }
+    if(element.href == winlocation){
+        console.log(element)
+        console.log(element.parentNode)
+        $(element.parentNode).addClass("active")
+    }});
+});
