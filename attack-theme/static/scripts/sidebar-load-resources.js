0422b9a405fe8f195bccadecec9f95fe753f601c
adpare <apare@mitre.org>
2023-09-18 16:53:49 -0400

    sidebar update. make calling function easier

--- attack-theme/static/scripts/sidebar-load-resources.js
+++ attack-theme/static/scripts/sidebar-load-resources.js
@@ -1,4 +1,6 @@
-$("#sidebars").load("/resources/sidebar-resources", function() {
+let mod_name = window.location.pathname.split("/")
+let mod_entry = "/" + mod_name[1] + "/sidebar-" + mod_name[1]
+$("#sidebars").load(mod_entry, function() {
     var navElements = document.querySelectorAll('.sidenav-head > a');
     var winlocation;
     navElements.forEach(function(element){
@@ -10,19 +12,25 @@ $("#sidebars").load("/resources/sidebar-resources", function() {
     }
     if(element.href == winlocation){
         $(element.parentNode).addClass("active")
+        var parentElement = $(element.parentNode);
+        var elementID = document.getElementById(parentElement[0].id)
+        elementID.scrollIntoView({ behavior: "smooth", block: "center" })
     }});
     var mediaQuery = window.matchMedia('(max-width: 47.9875rem)')
 
     function mobileSidenav(e) {
     if (e.matches) {
-      $('#sidebar-collapse').collapse('hide')
+      $('#sidenav-list').collapse('hide')
     }
     else{
-      $('#sidebar-collapse').collapse('show')
+      $('#sidenav-list').collapse('show')
     }
     }
     $(document).ready(function() {
         mobileSidenav(mediaQuery)
+        let sidenav = $(".sidenav-list");
+        let sidenav_active_elements = $(".sidenav .active");
+        sidenav_active_elements[0].scrollIntoView({ behavior: "smooth", block: "center" })
     });
 
     mediaQuery.addEventListener('change', mobileSidenav)
