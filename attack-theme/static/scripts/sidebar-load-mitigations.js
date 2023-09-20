42240d8236ef1f575cd0fba0f1239ea9cd5c40e3
adpare <apare@mitre.org>
2023-09-20 11:11:39 -0400

    code cleanup 1

--- attack-theme/static/scripts/sidebar-load-mitigations.js
+++ attack-theme/static/scripts/sidebar-load-mitigations.js
@@ -1,32 +1,30 @@
 $("#sidebars").load("/mitigations/sidebar-mitigations", function() {
-        var navElements = document.querySelectorAll('.sidenav-head > a');
-        var winlocation;
-        navElements.forEach(function(element){
-        if(!window.location.href.endsWith("/")){
-            winlocation = window.location.href + "/";
-        }
-        else{
-         winlocation = window.location.href
-        }
-        if(element.href == winlocation){
-            $(element.parentNode).addClass("active")
-        }});
-        var mediaQuery = window.matchMedia('(max-width: 47.9875rem)')
+  var navElements = document.querySelectorAll('.sidenav-head > a');
+  var winlocation;
+  navElements.forEach(function(element){
+  if(!window.location.href.endsWith("/")){
+    winlocation = window.location.href + "/";
+  }
+  else{
+    winlocation = window.location.href
+  }
+  if(element.href == winlocation){
+    $(element.parentNode).addClass("active")
+  }});
 
-        function mobileSidenav(e) {
-        if (e.matches) {
-          $('#sidebar-collapse').collapse('hide')
-        }
-        else{
-          $('#sidebar-collapse').collapse('show')
-        }
-        }
-        $(document).ready(function() {
-            mobileSidenav(mediaQuery)
-            let sidenav = $(".sidenav-list");
-            let sidenav_active_elements = $(".sidenav .active");
-            sidenav_active_elements[0].scrollIntoView({ block: 'nearest', inline: 'start' })
-        });
-    
-        mediaQuery.addEventListener('change', mobileSidenav)
+  var mediaQuery = window.matchMedia('(max-width: 47.9875rem)')
+  function mobileSidenav(e) {
+    if (e.matches) {
+      $('#sidebar-collapse').collapse('hide')
+    }
+    else{
+      $('#sidebar-collapse').collapse('show')
+    }
+  }
+  $(document).ready(function() {
+    mobileSidenav(mediaQuery)
+    let sidenav_active_elements = $(".sidenav .active");
+    sidenav_active_elements[0].scrollIntoView({ block: 'nearest', inline: 'start' })
+  });
+  mediaQuery.addEventListener('change', mobileSidenav)
 });
