4833c5a4ec501d278b875e177db290631b96efee
adpare <apare@mitre.org>
2023-09-27 16:08:14 -0400

    fixed error with mobile view

--- attack-theme/static/scripts/resizer.js
+++ attack-theme/static/scripts/resizer.js
@@ -32,20 +32,3 @@ function resizeSidebar_mouseupHandler() {
 
 resizer.addEventListener("mousedown", resizeSidebar_mousedownHandler);
 }
-
-//This code is for creating a collapsable sidebar for the mobile view
-var mediaQuery = window.matchMedia('(max-width: 47.9875rem)')
-
-function mobileSidenav(e) {
-  if (e.matches) {
-      $('#sidebar-collapse').collapse('hide')
-  }
-  else{
-      $('#sidebar-collapse').collapse('show')
-  }
-}
-$(document).ready(function() {
-  mobileSidenav(mediaQuery)
-});
-
-mediaQuery.addEventListener('change', mobileSidenav)
