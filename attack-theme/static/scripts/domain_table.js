9fa66204605ebec47a239b9780b2155260cb31ca
adpare <apare@mitre.org>
2023-10-13 10:22:27 -0400

    sonarcloud fix1

--- attack-theme/static/scripts/domain_table.js
+++ attack-theme/static/scripts/domain_table.js
@@ -1,19 +1,19 @@
 // this function filters table rows based on the selection
 function filter_row(selected) {
-    col_index = 3
+    var col_index = 3
     const rows = document.querySelectorAll("#ds-table tbody tr");
     let count = 0;
     rows.forEach((row) => {
         let row_count = 0
-        var row_visited = false;
-        row_data = row.querySelector("td:nth-child(" + col_index + ")").innerHTML
+        let row_visited = false;
+        var row_data = row.querySelector("td:nth-child(" + col_index + ")").innerHTML
         for(let i = 0; i<selected.length; i++){
-            filter_value = selected[i];
-            var display_row = true;
+            let filter_value = selected[i];
+            let display_row = true;
             if (row_data.indexOf(filter_value) == -1 && !row_visited) {
                 display_row = false;
             }
-            if (display_row == true) {
+            if (display_row) {
                 row.style.display = "table-row";
                 row_visited = "true";
                 row_count = row_count + 1;
@@ -25,13 +25,13 @@ function filter_row(selected) {
             count = count + 1;
         }
     })
-    filter_count = document.querySelector(".table-object-count")
+    var filter_count = document.querySelector(".table-object-count")
     filter_count.innerHTML = `Data Sources: ${count}`
 }
 
 $(document).ready(function() {
-    var arrow_up = document.getElementById("arrow-up-0");
-    var arrow_down = document.getElementById("arrow-down-0");
+    let arrow_up = document.getElementById("arrow-up-0");
+    let arrow_down = document.getElementById("arrow-down-0");
     arrow_down.style.display = "inline-block";
     arrow_up.style.display = "none";
     arrow_up = document.getElementById("arrow-up-1");
@@ -43,7 +43,7 @@ $(document).ready(function() {
 
 // this function determines which domain options were checked
 function showDomain() {
-    var selected = [];
+    let selected = [];
     if($("#filterMenu input:checked").length <= 0){
         $('#filterMenu input:checkbox').each(function() {
             selected.push($(this).attr('id'));
@@ -61,22 +61,22 @@ function showDomain() {
 
 // this function sorts the table based on either ID or Name
 function sortTable(col_no) {
-    var table = document.getElementById("ds-table");
-    var direction = "asc";
-    var table_switching = true;
-    var asc_direction = false;
-    var arrow_up = document.getElementById("arrow-up-"+col_no);
-    var arrow_down = document.getElementById("arrow-down-"+col_no);
+    let table = document.getElementById("ds-table");
+    let direction = "asc";
+    let table_switching = true;
+    let asc_direction = false;
+    let arrow_up = document.getElementById("arrow-up-"+col_no);
+    let arrow_down = document.getElementById("arrow-down-"+col_no);
     arrow_down.style.display = "inline-block";
     arrow_up.style.display = "none";
-    rows = table.rows;
+    var rows = table.rows;
     while (table_switching) {
         table_switching = false;
         if (direction == "desc"){
             for (let i = 1; i <= (rows.length - 1); i++) {
                 for (let j = 1; j <= (rows.length - i - 1); j++) {
-                    var x = rows[j].getElementsByTagName("TD")[col_no];
-                    var y = rows[j + 1].getElementsByTagName("TD")[col_no];
+                    let x = rows[j].getElementsByTagName("TD")[col_no];
+                    let y = rows[j + 1].getElementsByTagName("TD")[col_no];
                     if(x.innerText.toLowerCase() < y.innerText.toLowerCase()){
                         rows[j].parentNode.insertBefore(rows[j + 1], rows[j]);
                     }
@@ -88,8 +88,8 @@ function sortTable(col_no) {
         else{
             for (let i = 1; i <= (rows.length - 1); i++) {
                 for (let j = 1; j <= (rows.length - i - 1); j++) {
-                    var x = rows[j].getElementsByTagName("TD")[col_no];
-                    var y = rows[j + 1].getElementsByTagName("TD")[col_no];
+                    let x = rows[j].getElementsByTagName("TD")[col_no];
+                    let y = rows[j + 1].getElementsByTagName("TD")[col_no];
                     if(x.innerText.toLowerCase() > y.innerText.toLowerCase()){
                         rows[j].parentNode.insertBefore(rows[j + 1], rows[j]);
                         asc_direction = true;
