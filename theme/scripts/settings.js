aaf07fb062eda150ec47dbc99fe0e22ab95347b4
jondricek <jondricek@users.noreply.github.com>
2026-08-06 16:15:43 +0000

    deploy: 88365eb0fdc16271c95f5d22878addaca213853d

--- theme/scripts/settings.js
+++ theme/scripts/settings.js
@@ -1,3 +1,3 @@
 var base_url = "/";
-var build_uuid = "65ef5164-429b-98e2-e186-530404dd64d3";
-var tour_steps = {'matrix': 'matrices/enterprise', 'technique': 'techniques/T1548', 'subtechnique': 'techniques/T1548/001', 'relationships': {'step3': ['T1003-004', 5], 'step2': ['T1555', 3], 'obj_id': 'software/S0349'}};
+var build_uuid = "35737214-fa47-3aaf-b0f9-ac44398d09c6";
+var tour_steps = {'matrix': 'matrices/enterprise', 'technique': 'techniques/T1548', 'subtechnique': 'techniques/T1548/002', 'relationships': {'step3': ['T1003-004', 5], 'step2': ['T1555', 3], 'obj_id': 'software/S0349'}};
--- versions/v19/theme/scripts/settings.js
+++ versions/v19/theme/scripts/settings.js
@@ -1,3 +1,3 @@
 var base_url = "/versions/v19/";
-var build_uuid = "65ef5164-429b-98e2-e186-530404dd64d3";
+var build_uuid = "35737214-fa47-3aaf-b0f9-ac44398d09c6";
 var tour_steps = {};
